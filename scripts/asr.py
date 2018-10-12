f4114fcfcdd1efd1562f0f9e931e21b965595594
ricardoGrando <ricardogrando13@gmail.com>
2018-10-12 19:37:21 -0300

    asdfasd

--- scripts/asr.py
+++ scripts/asr.py
@@ -3,9 +3,12 @@ import rospy
 from std_msgs.msg import String
 import os
 from pocketsphinx import LiveSpeech, get_model_path
+import pyttsx
 
 model_path = get_model_path()
 print(model_path)
+
+engine = pyttsx.init()
 
 speech = LiveSpeech(
     verbose=False,
@@ -18,16 +21,56 @@ speech = LiveSpeech(
     dic=os.path.join(os.path.expanduser("~/catkin_ws")+"/src/ASR_Doris/", 'doris.dict')
 )
 
+def treat_message(message):
+    words = message.split()
+    message = ""
+    pub = False
+
+    if "today" in words:
+        if "day" in words:
+            message = "What day is today?"
+        else:
+            message = "What day is today?"
+
+    if "name" in words:
+        if "your" in words:
+            message = "What is your name?"
+        elif "my" in words:
+            message = "What is my name?"
+        else:
+            message = "What is my name?"
+
+    if "kitchen" in words:
+        if "go" in words:
+            message = "Go to the kitchen"
+        elif "where" in words:
+            message = "Where is the kitchen"
+        else:
+            message =  "Where is the kitchen"
+    
+    if "people" in words:
+        if "how many" in words:
+            message =  "How many peoople are"
+    
+    if message != "":
+        pub = True
+        print("Command: "+message)
+        engine.say(message)
+        engine.runAndWait()
+
+    return message, pub 
 def my_robot_talker():
     hello_pub = rospy.Publisher('doris/acr', String,queue_size=10)
     rospy.init_node('Speech_Recognition', anonymous=False)
     rate = rospy.Rate(1)
 
     while not rospy.is_shutdown():
-        for message in speech:
-            print(message)
-            rospy.loginfo(message)
-            hello_pub.publish(message)
+        for raw_message in speech:
+            print("Raw message: "+str(raw_message))
+            message, pub = treat_message(str(raw_message))
+            #rospy.loginfo(message)
+            if pub == True:
+                hello_pub.publish(message)
             rate.sleep()
 
 if __name__=='__main__':
